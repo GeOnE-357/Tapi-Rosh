555c0ced3690d1a6f8fd83746901696d96c16ae1
GeOnE <emedarias@gmail.com>
2020-08-09 19:49:47 -0300

    Agregados estilos generales, ilustraciones y funciones de juego basicas

--- js/juego.js
+++ js/juego.js
@@ -1,21 +1,20 @@
 const juego=document.getElementsByClassName("Nivel")[0];
 let puntos=document.getElementById("puntos");
 let vidas=document.getElementById("vidas");
+let pausa = false;
 let tapi = new Tapi(1);
-inicio();
+let spawn;
+let enemigos=[];
 
-
-function inicio()
+function Inicio()
 	{
-		tablero();
+		Tablero();
 		tapi.dibujar();
-		// Velocidad inicial ponerla en 800, y empezar a bajar de a 200.
-		// Velocidad Limites 800 - 400.
-		let spawn=setInterval(enemigos,800);
+		spawn=setInterval(CrearEnemigos,800);
 	}
 
 
-function tablero()
+function Tablero()
 	{
 		for (let i=0; i < 6; i++)
 			{
@@ -24,7 +23,7 @@ function tablero()
 						let div=document.createElement("div");
 						num=a+1;
 						num2=i+1;
-						div.className="col"+num+" fil"+num2;
+						div.className="col"+num+"-fil"+num2;
 						div.onclick=function(){tapi.mover(a+1)};
 						juego.appendChild(div);
 					}
@@ -32,7 +31,45 @@ function tablero()
 	}
 
 
+function CrearEnemigos()
+	{
+		let factu = new Factura(numero(5),numero(5));
+		factu.id=enemigos.length;
+		factu.dibujar();
+		factu.tiempo=setInterval(function(){factu.bajar()},factu.velocidad);
+		enemigos.push(factu);
+	}
+
+
+function Pausar()
+	{
+		if(pausa==false)
+			{
+				pausa=true;
+				clearInterval(spawn);
+				for(let i=0; i<enemigos.length;i++)
+					{
+						clearInterval(enemigos[i].tiempo);
+					}
+			}
+		else
+			{
+				pausa=false;
+				for(let i=0; i<enemigos.length;i++)
+					{
+						enemigos[i].tiempo=setInterval(function(){enemigos[i].bajar()},enemigos[i].velocidad)	
+					}
+				spawn=setInterval(CrearEnemigos,800);	
+			}
+	}
+
+function FinJuego()
+	{
+		Pausar();
+	}
+
 //---------------- Objetos Tapi ----------------
+
 
 function Tapi(col)
 	{
@@ -47,7 +84,7 @@ function Tapi(col)
 
 		function dibujar()
 			{
-				let bloque=document.getElementsByClassName("col"+this.columna+" fil"+this.fila)[0];
+				let bloque=document.getElementsByClassName("col"+this.columna+"-fil"+this.fila)[0];
 				let dibu=document.createElement("div");
 				dibu.className="tapi";
 				bloque.appendChild(dibu);	
@@ -55,68 +92,81 @@ function Tapi(col)
 
 		function mover(col)
 			{
-				borrar(this.columna, this.fila);
-				this.columna=col;
-				this.dibujar();
+				if(this.vidas>0 && pausa==false)
+					{
+						borrar(this.columna, this.fila, "tapi");
+						this.columna=col;
+						this.dibujar();
+					}
 			}
 	}
 
 
+
 //---------------- Objetos Factura ----------------
 
-function enemigos()
-	{
-		let factu = new Factura(numero(4),numero(5));
-		factu.dibujar();
-		factu.tiempo=setInterval(function(){factu.bajar()},factu.velocidad);
-	}
 
 function Factura(tip,col)
 	{
+		this.id=0;
 		this.tipo=tip;
 		this.fila=1;
 		this.columna=col;
 		this.puntos=5;
 		this.velocidad=200;
+		this.tiempo;
 
 		//Funciones del objeto
 		this.dibujar=dibujar;
 		this.bajar=bajar;
-		this.tiempo;
+		this.parar=parar;
 
 		function dibujar()
 			{
-				let bloque=document.getElementsByClassName("col"+this.columna+" fil"+this.fila)[0];
+				let bloque=document.getElementsByClassName("col"+this.columna+"-fil"+this.fila)[0];
 				let dibu=document.createElement("div");
-				dibu.className="factura";
+				dibu.className="factura fac"+this.tipo;
 				bloque.appendChild(dibu);
 			}
 		
 		function bajar()
 			{
-				borrar(this.columna, this.fila);
-				if(this.fila<5)
+				if(tapi.vidas>0 && pausa==false)
 					{
-						this.fila+=1;
-						this.dibujar();
-					}
-				else
-					{
-						if(tapi.columna==this.columna)
-							{
-								tapi.puntos+=this.puntos;
-								puntos.innerHTML=tapi.puntos;
-							}
-						else
+						borrar(this.columna, this.fila, "factura");
+						if(this.fila<5)
 							{
 								this.fila+=1;
 								this.dibujar();
-								tapi.vidas-=1;
-								setTimeout(borrar,100,this.columna,this.fila);
-								vidas.innerHTML=tapi.vidas;
 							}
-						clearInterval(this.tiempo);
-					}
+						else
+							{
+								if(tapi.columna==this.columna)
+									{
+										tapi.puntos+=this.puntos;
+										puntos.innerHTML=tapi.puntos;
+									}
+								else
+									{
+										this.fila+=1;
+										this.dibujar();
+										tapi.vidas-=1;
+										setTimeout(borrar,100,this.columna,this.fila,"factura");
+										vidas.innerHTML=tapi.vidas;
+										if(tapi.vidas==0)
+											{
+												FinJuego();
+											}
+									}
+								enemigos.splice(this.id, 1);
+								this.parar();
+							}
+				}
+			}
+
+		function parar()
+			{
+				clearInterval(this.tiempo);
 			}
 	}
 
@@ -133,15 +183,19 @@ function numero(max)
 	}
 
 
-function borrar(columna,fila)
+function borrar(columna,fila,tipo)
 	{
 		// Funcion para remover elementos en una columna "X" y fila "Y".
-		let bloque=document.getElementsByClassName("col"+columna+" fil"+fila)[0];
-		bloque.removeChild(bloque.firstChild);
+		let grilla="div.col"+columna+"-fil"+fila;
+		let bloque=document.querySelector(grilla);
+		let elemento=grilla+" div."+tipo;
+		let hijo=document.querySelector(elemento);
+		bloque.removeChild(hijo);
 	}
 
 function teclado(event)
 	{
+  		
   		if(event.keyCode==39 || event.keyCode==68)
   			{
   				if(tapi.columna<4)

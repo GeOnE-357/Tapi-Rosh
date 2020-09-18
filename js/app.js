fd2a6e5891732e2b255749d93b72f4f13cd203e7
GeOnE <emedarias@gmail.com>
2020-09-18 14:06:47 -0300

    agregada opcion de mostrar boton para instalar PWA

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+const instalar=document.querySelector(".instalar");
+let deferredPrompt;
+
 if('serviceWorker' in navigator)
 	{
 		console.log("Acepta service worker");
@@ -5,3 +8,26 @@ if('serviceWorker' in navigator)
 		.then((reg) => console.log("Service Worker Registrado",reg)) /*Si el registro sucede, entoces realiza esto.*/
 		.catch((err) => console.log("Service Worker No Registrado",err)); /*Sino, atrapa el error e informa.*/
 	}
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Previene que la opcion de instalar se muestre.
+  e.preventDefault();
+  // Guarda el evento para poder ser disparado cuando quiera.
+  deferredPrompt = e;
+  instalar.classList.remove("oculto");
+});
+
+const InstalarPwa = () => {
+    instalar.classList.add("oculto");
+    // Muestra la opcion de instalar.
+    deferredPrompt.prompt();
+    // Espera a la opcion del usuario.
+    deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        deferredPrompt = null;
+      });
+  }
--- js/touch.js
+++ js/touch.js
@@ -1,10 +1,9 @@
 /*Pequeño js para convertir todos los clicks que realiza el usuario en hover
 , para que vea las animaciones de la pagina/juego.*/ 
+const hover= document.querySelectorAll(".Hov");
 
 //Funcion que atrapa todos los eventos en el documento html
-let hover= document.querySelectorAll(".Hov");
-
-function EventHandler(e)
+const EventHandler =(e)=>
 	{
 		var evento = e || window.event;
 		var objetivo = evento.target || evento.srcElement;
@@ -26,23 +25,23 @@ function EventHandler(e)
 		}
 
 
-function EstadoHover(objetivo)
+const EstadoHover=(objetivo)=>
 	{
 		objetivo.classList.add("presionado");
 		setTimeout(SacarHover,300,objetivo);
 	}
 
 
-function SacarHover(objetivo)
+const SacarHover=(objetivo)=>
 	{
 		objetivo.classList.remove("presionado");
-		if(objetivo.classList.contains("Es0")){cambioEscena(0);};
-		if(objetivo.classList.contains("Es1")){cambioEscena(1);};
-		if(objetivo.classList.contains("Es2")){cambioEscena(2);};
-		if(objetivo.classList.contains("Es3")){cambioEscena(3);};
-		if(objetivo.classList.contains("Pau")){Pausar();};
-		if(objetivo.classList.contains("Pri")){Principio();};
-		if(objetivo.classList.contains("Ins")){console.log("Ins")};
-		if(objetivo.classList.contains("creador")){window.open(objetivo.href);};
-		if(objetivo.classList.contains("Lin")){location.assign(objetivo.href);};	
+		if(objetivo.classList.contains("Es0")){cambioEscena(0)};
+		if(objetivo.classList.contains("Es1")){cambioEscena(1)};
+		if(objetivo.classList.contains("Es2")){cambioEscena(2)};
+		if(objetivo.classList.contains("Es3")){cambioEscena(3)};
+		if(objetivo.classList.contains("Pau")){Pausar()};
+		if(objetivo.classList.contains("Pri")){Principio()};
+		if(objetivo.classList.contains("Ins")){InstalarPwa()};
+		if(objetivo.classList.contains("creador")){window.open(objetivo.href)};
+		if(objetivo.classList.contains("Lin")){location.assign(objetivo.href)};	
 	}

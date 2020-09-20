ac3b682ca6ec58253d4e5db84546e4d7253cb79f
GeOnE <emedarias@gmail.com>
2020-09-20 18:19:48 -0300

    Arreglado error 404

--- sw.js
+++ sw.js
@@ -1,6 +1,8 @@
-const nombreCache="sitio-cache";
-const dinamicoCache="sitio-dinamico";
-const elementos=["/Tapi-Rosh/","/Tapi-Rosh/index.html","/Tapi-Rosh/fallback.html","/Tapi-Rosh/css/estilo.css","/Tapi-Rosh/css/animate.css","/Tapi-Rosh/js/app.js","/Tapi-Rosh/js/escenas.js","/Tapi-Rosh/js/juego.js","/Tapi-Rosh/js/touch.js","/Tapi-Rosh/manifest.webmanifest","/Tapi-Rosh/img/creditos/icon-art.webp","/Tapi-Rosh/img/creditos/icon-down.webp","/Tapi-Rosh/img/creditos/icon-prog.webp","/Tapi-Rosh/img/tramas/patron200x200blanco.webp","/Tapi-Rosh/img/jugar/a.webp","/Tapi-Rosh/img/jugar/b.webp","/Tapi-Rosh/img/jugar/izq.webp","/Tapi-Rosh/img/jugar/der.webp","/Tapi-Rosh/img/jugar/mano.webp","/Tapi-Rosh/img/back1.webp","/Tapi-Rosh/img/fac1.webp","/Tapi-Rosh/img/fac2.webp","/Tapi-Rosh/img/fac3.webp","/Tapi-Rosh/img/fac4.webp","/Tapi-Rosh/img/fac5.webp","/Tapi-Rosh/img/logo.webp","/Tapi-Rosh/img/ups.webp","/Tapi-Rosh/img/tapi.webp","https://fonts.googleapis.com/css2?family=Rowdies&display=swap"];
+const version="0.0.1";
+const nombreCache="sitio-cache-";
+document.querySelector(".version").innerHTML="<strong>ver."+version+"</strong>";
+const dinamicoCache="sitio-dinamico-0.0.1";
+const elementos=["/Tapi-Rosh/","/Tapi-Rosh/index.html","/Tapi-Rosh/404.html","/Tapi-Rosh/css/estilo.css","/Tapi-Rosh/css/animate.css","/Tapi-Rosh/js/app.js","/Tapi-Rosh/js/escenas.js","/Tapi-Rosh/js/juego.js","/Tapi-Rosh/js/touch.js","/Tapi-Rosh/manifest.webmanifest","/Tapi-Rosh/img/creditos/icon-art.webp","/Tapi-Rosh/img/creditos/icon-down.webp","/Tapi-Rosh/img/creditos/icon-prog.webp","/Tapi-Rosh/img/tramas/patron200x200blanco.webp","/Tapi-Rosh/img/jugar/a.webp","/Tapi-Rosh/img/jugar/b.webp","/Tapi-Rosh/img/jugar/izq.webp","/Tapi-Rosh/img/jugar/der.webp","/Tapi-Rosh/img/jugar/mano.webp","/Tapi-Rosh/img/back1.webp","/Tapi-Rosh/img/fac1.webp","/Tapi-Rosh/img/fac2.webp","/Tapi-Rosh/img/fac3.webp","/Tapi-Rosh/img/fac4.webp","/Tapi-Rosh/img/fac5.webp","/Tapi-Rosh/img/logo.webp","/Tapi-Rosh/img/ups.webp","/Tapi-Rosh/img/tapi.webp","https://fonts.googleapis.com/css2?family=Rowdies&display=swap"];
 
 //Funcion para limitar el cache
 const limiteCache = (nombre, tamaño)=> {
@@ -15,33 +17,15 @@ const limiteCache = (nombre, tamaño)=> {
 
 //Instalar el service worker.
 self.addEventListener("install", evt => {
-	console.log("El service worker se instalo.");
 	//1°-Guardamos en el cache, los elementos predefinidos de la pagina. Css, Javascripts y HTML.
-	evt.waitUntil(caches.open(nombreCache).then((cache) => {
+	evt.waitUntil(caches.open(nombreCache+version).then((cache) => {
 					cache.addAll(elementos);
 				})
 		);
 });
 
-
-//Activar el service worker.
-self.addEventListener("activate", evt =>{
-		//console.log("El service worker se activo.");
-		//3°-Chequearemos la version de cache, borraremos la version obsoleta.
-		evt.waitUntil(
-			caches.keys().then(keys => {
-				console.log(keys);
-				return Promise.all(keys
-					.filter(key => key !== nombreCache && key !== dinamicoCache)
-					.map(key => caches.delete(key))
-				)
-			})
-		);
-	});
-
 //Eventos Fetch (fetch request o pedido de busqueda).
 self.addEventListener("fetch", evt =>{
-	//console.log("Se atrapo el evento: ",evt);
 	//2°-Atrapamos los pedidos, para que los busque en el cache.
 	evt.respondWith(
 		caches.match(evt.request).then(cacheRes => {
@@ -59,4 +43,20 @@ self.addEventListener("fetch", evt =>{
 		})
 	);
 });
+
+//Activar el service worker.
+self.addEventListener("activate", evt =>{
+		//3°-Chequearemos la version de cache, borraremos la version obsoleta.
+		evt.waitUntil(
+			caches.keys().then(keys => {
+				console.log(keys);
+				return Promise.all(keys
+					.filter(key => key !== nombreCache && key !== dinamicoCache)
+					.map(key => caches.delete(key))
+				)
+			})
+		);
+	});
+
+
 
